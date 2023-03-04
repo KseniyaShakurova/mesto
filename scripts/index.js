82f21c6aa1f83749b31ffc7a37f5da46ead7785c
Kseniya Shakurova <6275751@mail.ru>
2023-03-04 09:24:55 +0300

    на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,18 @@ let popup = document.querySelector('.popup')
 let buttonOpen = document.querySelector('.profile__button-edit');
 let buttonClose = document.querySelector('.popup__btn');
 let formElement = document.querySelector('.popup__container');
-let nameImput = document.querySelector('.popup__name')
-let jobInput = document.querySelector('.popup__job');
-let buttonSave = document.querySelector('.popup__save');
+let nameImput = document.querySelector('.popup__info_input_name')
+let jobInput = document.querySelector('.popup__info_input_job');
 let nameSave = document.querySelector('.profile__title');
 let jobSave = document.querySelector('.profile__subtitle');
 
-buttonOpen.addEventListener ('click', function() {
+function btnOpen() {
     popup.classList.add('popup_opened');
     
-})
+    nameImput.textContent = nameSave.value;
+    jobInput.textContent = jobSave.value;
+    
+}
 
 function btnClose() {   
     popup.classList.remove('popup_opened');
@@ -22,18 +24,18 @@ function btnClose() {
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
 
-     nameImput.innerText = nameSave.value;
-     jobInput.innerText = jobSave.value;
      let formName = nameImput.value;
      let formJob = jobInput.value;
      nameSave.textContent = formName;
      jobSave.textContent = formJob;
     
      btnClose();
+     
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 buttonClose.addEventListener ('click', btnClose);
+buttonOpen.addEventListener ('click' , btnOpen);
 
 
 
